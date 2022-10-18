e806c3e553778ea26a40e769322b2c063667c3bc
Bhumi Shah <bhumi1102@gmail.com>
2022-10-18 14:14:59 -0500

    clean up jobs controller

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,16 +2,13 @@ class JobsController < ApplicationController
   before_action :set_job, only: %i[ show edit update destroy ]
   before_action :authenticate_user!
 
-  # GET /jobs or /jobs.json
   def index
     @jobs = Job.all
   end
 
-  # GET /jobs/1 or /jobs/1.json
   def show
   end
 
-  # GET /jobs/new
   def new
     html = render_to_string(partial: 'form', locals: { job: Job.new })
     render operations: cable_car
@@ -19,7 +16,6 @@ class JobsController < ApplicationController
       .text_content('#slideover-header', text: 'Post a new job')
   end
 
-  # GET /jobs/1/edit
   def edit
     html = render_to_string(partial: 'form', locals: { job: @job })
     render operations: cable_car
@@ -27,7 +23,6 @@ class JobsController < ApplicationController
       .text_content('#slideover-header', text: 'Update job')
   end
 
-  # POST /jobs or /jobs.json
   def create
     @job = Job.new(job_params)
     @job.account = current_user.account
@@ -44,7 +39,6 @@ class JobsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /jobs/1 or /jobs/1.json
   def update
     if @job.update(job_params)
       html = render_to_string(partial: 'job', locals: { job: @job })
@@ -58,7 +52,6 @@ class JobsController < ApplicationController
     end
   end
 
-  # DELETE /jobs/1 or /jobs/1.json
   def destroy
     @job.destroy
 
